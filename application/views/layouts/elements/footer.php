a1714b62954e370d0439586bdfa8b2338b47ea3c
Mohamad Zaki <mzm@kpkt.gov.my>
2015-04-06 12:05:13 +0800

    update

--- application/views/layouts/elements/footer.php
+++ application/views/layouts/elements/footer.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-    <p> <img width="32" height="32" src="https://avatars0.githubusercontent.com/u/11433058?v=3&amp;s=80" alt="@kpkt"> &copy; https://github.com/kpkt/ci_crud <?php echo date('Y') ?></p>
+    <p> <img width="32" height="32" src="<?php echo base_url() ?>img/11433058.png" alt="@kpkt"> &copy; https://github.com/kpkt/ci_crud <?php echo date('Y') ?></p>
 </footer>
